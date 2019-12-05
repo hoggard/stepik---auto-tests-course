86ec53dbdcacb258124c1bd1a99c9bed0a6badff
Hoggard <hoggard22@mail.ru>
2019-12-05 13:27:12 +0300

    renamed folder and files

--- Lessons/Scripts/pip3.8-script.py
+++ Lessons/Scripts/pip3.8-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\amahin\PycharmProjects\Selenium\venv\Scripts\python.exe
+#!C:\Users\amahin\PycharmProjects\Selenium\Lessons\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.8'
 __requires__ = 'pip==19.0.3'
 import re
